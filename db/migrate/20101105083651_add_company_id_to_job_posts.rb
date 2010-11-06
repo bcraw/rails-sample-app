d2b612440a3a07e1d36fb908018fda2aae9998a4
bcraw <bcrawfish@gmail.com>
2010-11-06 13:44:57 -0700

    removed redundant db migrate files

--- db/migrate/20101105083651_add_company_id_to_job_posts.rb
+++
@@ -1,9 +0,0 @@
-class AddCompanyIdToJobPosts < ActiveRecord::Migration
-  def self.up
-    add_column :job_posts, :company_id, :integer
-  end
-
-  def self.down
-    remove_column :job_posts, :company_id, :integer
-  end
-end
